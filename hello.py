163cc2d11aec6ff5a3bd182f6de875dbb9f71b7c
Arslan Ovezov <arslan.datascience@gmail.com>
2025-03-05 01:18:50 +0500

    ars3

--- hello.py
+++ hello.py
@@ -1,4 +1,15 @@
 
+# Ask user for their name
+name = input("What is your name? ")
 
-name = input("what is your name? ")
-print("hello, Arslan")
+# Say hello to user
+print("Hello, " + name + "!")
+
+# Ask user for their age
+age = input("How old are you? ")
+
+# Say how old user will be in 10 years
+age = int(age) + 10 
+
+print("In 10 years, you will be " + str(age) + " years old!")
+
