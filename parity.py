9a66bec69b5f9220e5f8e4899888567aecf4a20a
Arslan Ovezov <arslan.datascience@gmail.com>
2025-03-08 22:35:26 +0500

    ars6

--- parity.py
+++ parity.py
@@ -1,8 +1,14 @@
-x = int(input("What's x? "))
+def main():
+    x = int(input("What's x? "))
+    if is_even(x):
+        print("even")
+    else:
+        print("odd")
 
-if x % 2 == 0: 
-    print("Even")
-else: 
-    print("Odd")
+def is_even(n):
+    return n % 2 == 0
+        
+main()
+        
     
     
