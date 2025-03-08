304004dbebf4e673b9bd50b0a29c273862945167
Arslan Ovezov <arslan.datascience@gmail.com>
2025-03-08 22:42:06 +0500

    ars7

--- house.py
+++ house.py
@@ -1,8 +1,18 @@
 name = input("What is your name? ")
 
-if name == "Harry" or name == "Hermione" or name == "Ron":
-    print("Gryffindor!")
-elif name == "Draco":
-    print("Slytherin!")
-else:
-    print("Who?")
+# if name == "Harry" or name == "Hermione" or name == "Ron":
+#     print("Gryffindor!")
+# elif name == "Draco":
+#     print("Slytherin!")
+# else:
+#     print("Who?")
+
+# another way to write the above code is:
+
+match name:
+    case "Harry" | "Hermione" | "Ron":
+        print("Gryffindor!")
+    case "Draco":
+        print("Slytherin!")
+    case _:
+        print("Who?")
