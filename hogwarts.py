602279a29c397ccdecf36714bb170e34ecd8d92c
Arslan Ovezov <arslan.datascience@gmail.com>
2025-03-16 00:27:03 +0500

    ars2

--- hogwarts.py
+++ hogwarts.py
@@ -3,4 +3,26 @@
 # for i in range(len(students)):
 #     print(i + 1, students[i])
 
+# students = {"Hermione": 'Gryffindor', 
+#             'Harry':'Gryffindor', 
+#             'Ron': 'Gryffindor', 
+#             'Draco': 'Slytherin'
+#             }
+
+# for student in students:
+#     print(student, students[student], sep=", ")
+
+students = [
+    {"name": "Hermione","house": "Gryffindor","patronus": "Otter"},
+    {"name": "Herry", "house": "Gryffindor", "patronus": "Stag"},
+    {"name": "Ron", "house": "Gryffindor", "patronus": "Jack Russll terrier"},
+    {"name": "Draco", "house": "Slytherin", "patronus": None},
+]
+
+for student in students:
+    print(student["name"], student["house"], student["patronus"], sep=", ")
+
+
+
+
 
