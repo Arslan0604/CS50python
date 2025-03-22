2238d44887069987334a63e2de4fec2fbdfff8eb
Arslan Ovezov <arslan.datascience@gmail.com>
2025-03-22 16:42:43 +0500

    ars5

--- calculator.py
+++ calculator.py
@@ -1,16 +1,11 @@
-x = float(input("What's x? ")) 
-y = float(input("What's y? "))
-
-z = x / y 
-
-print(z)
-
 def main():
-    x = float(input("What's x? ")) 
-    print("x squared is", square(x)) 
+    x = int(input("What's x? "))
+    print("x squared is", square(x))
     
 def square(n):
-    return n + n   
+    return n + n
+
 main()
 
-
+if __name__ == "__main__":
+    main()
