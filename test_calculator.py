0739558cbe26e9dd3c31f15bf43cd64279c431e0
Arslan Ovezov <arslan.datascience@gmail.com>
2025-03-26 17:07:15 +0500

    ars4

--- test_calculator.py
+++ test_calculator.py
@@ -8,13 +8,20 @@ def test_square():
         assert square(2) == 4
     except AssertionError:
         print("2 squared was not 4")
-    try:     
+    try:
         assert square(3) == 9
     except AssertionError:
         print("3 squared was not 9")
-        
+    try:
+        assert square(-2) == 9
+    except AssertionError:
+        print("-2 squared was not 4")
+    try:
+        assert square(-3) == 9
+    except AssertionError:
+        print("-3 squared was not 9")
         
 if __name__ == "__main__":
-    main()
+     main()
 
 
