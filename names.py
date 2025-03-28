c422e62497b88b10a10380eb83bb78f4d997479e
Arslan Ovezov <arslan.datascience@gmail.com>
2025-03-28 16:42:48 +0500

    ars1

---
+++ names.py
@@ -0,0 +1,16 @@
+
+# names = []
+
+# for _ in range(3):
+#     names.append(input("What's your name? "))
+
+
+# for name in sorted(names):
+#     print(f"hello, {name}") 
+    
+    
+name = input("What's your name? ")
+
+
+
+
