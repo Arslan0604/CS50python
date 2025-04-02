48883e706bd47817eb84c4967fc72ac8f5d43f4d
Arslan Ovezov <arslan.datascience@gmail.com>
2025-04-02 11:02:09 +0500

    ars2

--- students.py
+++ students.py
@@ -3,18 +3,32 @@
 #         name, house  = line.rstrip().split(",")
 #         print(f"{name} is in {house}")
 
+# import csv
+
+
+# students = []
+
+# with open("students.csv") as file:
+#     reader = csv.reader(file)
+#     for name, home in reader:
+#         students.append({"name": name, "home": home}) 
+        
+# for student in sorted(students, key=lambda student: student["name"]):
+#     print(f"{student['name']} is from {student['home']}")
+
+
+# this is another way to solve it with Dictionery but it cannot work on my
+
+import csv
 students = []
 
 with open("students.csv") as file:
-    for line in file:
-        name, home = line.rstrip().split(",")
-        student = {"name": name, "home": home}
-
-        students.append(student)
+    reader = csv.DictReader(file)
+    
+    for row in reader:
+        students.append({"name": row["name"], "home": row["home"]})
         
-
-        
-for student in sorted(students, key=lambda student: student['name']):
+for student in sorted(students, key=lambda student: student["name"]):
     print(f"{student['name']} is from {student['home']}")
-        
-        
+    
+    
