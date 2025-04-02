3325177b9959478975845462ca8d226f470f2b33
Arslan Ovezov <arslan.datascience@gmail.com>
2025-04-02 09:11:39 +0500

    ars1

--- students.py
+++ students.py
@@ -7,15 +7,14 @@ students = []
 
 with open("students.csv") as file:
     for line in file:
-        name, house = line.rstrip().split(",")
-        student = {"name": name, "house": house}
+        name, home = line.rstrip().split(",")
+        student = {"name": name, "home": home}
 
         students.append(student)
         
-def get_house(student):
-    return student['house']
+
         
-for student in sorted(students, key=get_house, reverse=True):
-    print(f"{student['name']} is in {student['house']}")
+for student in sorted(students, key=lambda student: student['name']):
+    print(f"{student['name']} is from {student['home']}")
         
         
