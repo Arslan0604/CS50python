a7a67d55128e06c8fee155d58c54cc0acfd2a7d4
Arslan Ovezov <arslan.datascience@gmail.com>
2025-04-04 20:35:20 +0500

    ars1

--- students.py
+++ students.py
@@ -19,16 +19,35 @@
 
 # this is another way to solve it with Dictionery 
 
-import csv
-students = []
+# import csv
+# students = []
 
-with open("students.csv") as file:
-    reader = csv.DictReader(file)
+# with open("students.csv") as file:
+#     reader = csv.DictReader(file)
     
-    for row in reader:
-        students.append({"name": row["name"], "home": row["home"]})
+#     for row in reader:
+#         students.append({"name": row["name"], "home": row["home"]})
         
-for student in sorted(students, key=lambda student: student["name"]):
-    print(f"{student['name']} is from {student['home']}")
+# for student in sorted(students, key=lambda student: student["name"]):
+#     print(f"{student['name']} is from {student['home']}")
     
+    
+# import csv
+
+# name = input('What is your name? ')
+# home = input('Where is your home? ')
+
+# with open("students.csv", "a") as file: # v csv zapisyvaetsya 
+#     writer = csv.writer(file)
+#     writer.writerow([name, home])
+    
+import csv
+
+name = input('What is your name? ')
+home = input('Where is your home? ')
+
+with open("students.csv", "a") as file: # v csv zapisyvaetsya 
+    writer = csv.DictWriter(file, fieldnames=['name', 'home'])
+    writer.writerow({'name': name , 'home': home})   
+
     
