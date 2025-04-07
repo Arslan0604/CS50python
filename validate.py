3a59fc4f59a8e81cd77336e097f544d3a9a838c1
Arslan Ovezov <arslan.datascience@gmail.com>
2025-04-07 22:54:54 +0500

    ars2

--- validate.py
+++ validate.py
@@ -2,7 +2,7 @@ import re
 
 email = input("What is you email? ").strip()
 
-if re.search(r"^.+@.+\.edu$", email):
+if re.search(r"^[^@]+@[^@]+\.edu$", email):
     print("Valid email")
 else:
     print("INvalid email")
