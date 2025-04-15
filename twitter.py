3385fe5748b5148aff6c0c53f48562370039cd5d
Arslan Ovezov <arslan.datascience@gmail.com>
2025-04-15 13:51:44 +0500

    ars2

--- twitter.py
+++ twitter.py
@@ -1,5 +1,12 @@
+import re
+
+
 url = input("URL: ").strip()
 
-username = url.replace("https://github.com/", "")
+# username = url.replace("https://github.com/", "")
 
-print(f"Username: {username}")
+# username = url.removeprefix("https://github.com/")
+
+if matches := re.search(r"^https?://(?:www\.)?github\.com/([a-z0-9_]+)", url, re.IGNORECASE):
+    
+    print(f"Username:", matches.group(1))
