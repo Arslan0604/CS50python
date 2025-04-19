a722c3c0e56550d3bc86c2147f2064e4a44d1d33
Arslan Ovezov <arslan.datascience@gmail.com>
2025-04-19 22:32:21 +0500

    ars6

--- student.py
+++ student.py
@@ -1,13 +1,16 @@
+class Student:
+    pass
+
+
 def main():
     student = get_student()
-    if student["name"] == 'Padma':
-        student['house'] = 'Jonny'
-    print(f"{student['name']} from {student['house']}")
+    print(f"{student.name} from {student.house}")
     
 def get_student():
-    name = input("Name: ")
-    house = input("House: ")
-    return {'name': name, 'house': house}
+    student = Student()
+    student.name = input("Name: ")
+    student.house = input("House: ")
+    return student
 
 if __name__ == "__main__":
     main()
