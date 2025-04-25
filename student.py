83adaade90905de2eacb686240fe593a9bc8501c
Arslan Ovezov <arslan.datascience@gmail.com>
2025-04-25 23:19:37 +0500

    ars2

--- student.py
+++ student.py
@@ -18,7 +18,8 @@ def main():
     
 def get_student():
     name = input("Name: ")
-    house = input("House:")
+    house = input("House: ")
+    patronus = input("Patronus: ")
     return Student(name, house)
 
 if __name__ == "__main__":
