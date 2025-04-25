acf03ab63cb44f6f5e86f327922f339860a3a0c9
Arslan Ovezov <arslan.datascience@gmail.com>
2025-04-25 23:18:37 +0500

    ars1

--- student.py
+++ student.py
@@ -1,11 +1,12 @@
 class Student:
-    def __init__(self, name, house):
+    def __init__(self, name, house, patronus):
         if not name:
             raise ValueError("Missing name")
         if house not in ["Gryffindor", "Hufflepuff", "Ravenclaw", "Slytherin"]:
             raise ValueError("Invalid house")
         self.name = name 
         self.house = house
+        self.patronus = patronus
         
     def __str__(self):
         return f"{self.name} from {self.house}"
