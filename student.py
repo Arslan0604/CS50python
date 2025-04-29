dc8c8b75b7e96c5c5494dfbec1c526d943aa19b4
Arslan Ovezov <arslan.datascience@gmail.com>
2025-04-29 20:55:12 +0500

    ars2

--- student.py
+++ student.py
@@ -14,29 +14,7 @@ class Student:
         return cls(name, house)
         
     
-    @property
-    def name(self):
-        return self._name
-    
-    @name.setter
-    def name(self, name):
-        if not name:
-            raise ValueError("Missing name")
-        self._name = name
-        
-    
-    @property
-    def house(self):
-        return self._house 
-    
-    @house.setter
-    def house(self, house):
-        if house not in ["Gryffindor", "Hufflepuff", "Ravenclaw", "Slytherin"]:
-            raise ValueError("Invalid house")
-        self._house = house
-    
-  
-      
+
 def main():
     student = get_student()
     student._house = "Number Four, Privet Drive"
