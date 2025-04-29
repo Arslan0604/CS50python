d64163a039533814cd69518d2259dcbcc2d5ecee
Arslan Ovezov <arslan.datascience@gmail.com>
2025-04-29 21:45:33 +0500

    ars6

--- vault.py
+++ vault.py
@@ -3,3 +3,21 @@ class Vault:
         self.galleons = galleons
         self.sickles = sickles
         self.knuts = knuts
+        
+    def __str__(self):
+        return f"{self.galleons} Galleons, {self.sickles} Sickles, {self.knuts} Knuts"
+        
+    def __add__(self, other):
+        gallions = self.galleons + other.galleons
+        sickles = self.sickles + other.sickles
+        knuts = self.knuts + other.knuts
+        return Vault(gallions, sickles, knuts)
+        
+potter = Vault(100, 50, 25)
+print(potter)
+
+weasley = Vault(23, 89, 32)
+print(weasley)
+
+total = potter + weasley
+print(total)
