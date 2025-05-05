301192ec1d4786053629359ef4d2773bef97e2ae
Arslan Ovezov <arslan.datascience@gmail.com>
2025-05-05 14:33:21 +0500

    ars6

--- meows.py
+++ meows.py
@@ -1,2 +1,16 @@
-for i in range(3):
-    print("meow")
+# class Cat:
+#     MEOWS = 3 
+    
+#     def meow(self):
+#         for _ in range(self.MEOWS):
+#             print("meow")
+            
+# cat = Cat()
+# cat.meow()
+
+def meow(n: int):
+    for _ in range(n):
+        print("meow")
+      
+number: int = int(input("Number of meows: "))
+meow(number)
