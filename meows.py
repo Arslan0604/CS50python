c2a352c354fcf031a64e616bbadbdf19150ba09e
Arslan Ovezov <arslan.datascience@gmail.com>
2025-05-07 22:57:52 +0500

    ars4

--- meows.py
+++ meows.py
@@ -11,21 +11,32 @@
 # cat.meow()
 
 # this is second version of this code about meows
-def meow(n: int) -> str:
-    """
-    Meow n times.
+# def meow(n: int) -> str:
+#     """
+#     Meow n times.
     
-    :param n: Number of meows
-    :type n: int
-    :raise ValueError: if n is negative
-    :return: A string of n meows, one per line
-    :rtype: str
-    """
-    return "meow\n" * n
+#     :param n: Number of meows
+#     :type n: int
+#     :raise ValueError: if n is negative
+#     :return: A string of n meows, one per line
+#     :rtype: str
+#     """
+#     return "meow\n" * n
 
       
-number: int = int(input("Number of meows: "))
-meows: str = meow(number)
+# number: int = int(input("Number of meows: "))
+# meows: str = meow(number)
 
-print(meows, end="")
+# print(meows, end="")
+ 
+import sys
+ 
+if  len(sys.argv) == 1:
+     print("meow")
+elif len(sys.argv) == 3 and sys.argv[1] == "-n":
+    n = int(sys.argv[2])
+    for _ in range(n):
+        print("meow")
+else:
+     print("usage: meows.py")
  
