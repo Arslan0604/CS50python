5c2e44d1d90c9970a1e3e7f43cbc2f9a6d9633cb
Arslan Ovezov <arslan.datascience@gmail.com>
2025-05-08 18:10:20 +0500

    ars2

--- meows.py
+++ meows.py
@@ -45,7 +45,7 @@
 import argparse
 
 parser = argparse.ArgumentParser(description="Meow line a cat")
-parser.add_argument("-n", help="number of times to meow")
+parser.add_argument("-n", default=1, help="number of times to meow", type=int)
 args = parser.parse_args()
 
 for _ in range(int(args.n)):
