e835dec1e03b7d54ffd69857bc911d19a73e832c
Arslan Ovezov <arslan.datascience@gmail.com>
2025-05-08 18:18:23 +0500

    ars3

---
+++ unpack.py
@@ -0,0 +1,11 @@
+# first, _ = input("what's your name? ").split(" ")
+# print(f"Hello, {first}")
+
+def total(galleons, sickles, knuts):
+    return (galleons * 17 + sickles) * 29 + knuts
+
+
+coins = [100, 50, 25]
+
+
+print(total(100, 50, 25), "Knuts")
