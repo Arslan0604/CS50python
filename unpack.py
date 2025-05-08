b8241005c0576c03357846e7968db48b29f77dcf
Arslan Ovezov <arslan.datascience@gmail.com>
2025-05-08 18:48:01 +0500

    ars7

--- unpack.py
+++ unpack.py
@@ -3,14 +3,20 @@
 
 
 # unpacking process below
-def total(galleons, sickles, knuts):
-    return (galleons * 17 + sickles) * 29 + knuts
+# def total(galleons, sickles, knuts):
+#     return (galleons * 17 + sickles) * 29 + knuts
 
 
-# coins = [100, 50, 25]
-# print(total(*coins), "Knuts")
+# # coins = [100, 50, 25]
+# # print(total(*coins), "Knuts")
 
-# old school unpacking
+# # old school unpacking
 
-coins = {"galleons": 100, "sickles": 50, "knuts": 25}
-print(total(**coins), "Knuts")
+# coins = {"galleons": 100, "sickles": 50, "knuts": 25}
+# print(total(**coins), "Knuts")
+
+def f(*args, **kwargs):
+    print("Named:", kwargs)
+    
+    
+f(galleons=100, sickles=50, knuts=25)
