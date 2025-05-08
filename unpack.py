11f4b9f88fbbc76238931abd321106f6248659be
Arslan Ovezov <arslan.datascience@gmail.com>
2025-05-08 18:40:02 +0500

    ars6

--- unpack.py
+++ unpack.py
@@ -7,7 +7,10 @@ def total(galleons, sickles, knuts):
     return (galleons * 17 + sickles) * 29 + knuts
 
 
-coins = [100, 50, 25]
+# coins = [100, 50, 25]
+# print(total(*coins), "Knuts")
 
+# old school unpacking
 
-print(total(*coins), "Knuts")
+coins = {"galleons": 100, "sickles": 50, "knuts": 25}
+print(total(**coins), "Knuts")
