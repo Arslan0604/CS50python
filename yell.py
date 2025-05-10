fc4c20f834bb8f74a58fa113766485cb90c20fdb
Arslan Ovezov <arslan.datascience@gmail.com>
2025-05-10 19:19:57 +0500

    ars6

--- yell.py
+++ yell.py
@@ -3,10 +3,8 @@ def main():
     
     
 def yell(*words):
-    uppercased = map(str.upper, words)
-    
-                        
-        
+    uppercased = [word.upper() for word in words]
+         
     print(*uppercased)
     
     
