f4e75184679cb00a80030a19e2149b8aa4a4f640
Arslan Ovezov <arslan.datascience@gmail.com>
2025-05-10 19:55:57 +0500

    ars8

--- gryffindors.py
+++ gryffindors.py
@@ -4,3 +4,11 @@ students = [
     {"name": "Draco", "house": "Slytherin"},
     {"name": "Ron", "house": "Gryffindor"},
 ]
+
+
+gryffindors = [
+    student["name"] for student in students if student["house"] == "Gryffindor"
+]
+
+for gryffindor in sorted(gryffindors):
+    print(gryffindor)
