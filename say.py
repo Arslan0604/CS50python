0e9f2abc0ed8f0bfcaca032210ca14ea9f660a67
Arslan Ovezov <arslan.datascience@gmail.com>
2025-05-11 17:15:02 +0500

    ars8

--- say.py
+++ say.py
@@ -1,6 +1,9 @@
-import sys
+import cowsay
+import pyttsx3 
 
-from sayings import goodbye
 
-if len(sys.argv) == 2:
-    goodbye(sys.argv[1])
+engine = pyttsx3.init()
+this = input("What's this? ")
+cowsay.cow(this)
+engine.say(this)
+engine.runAndWait()
