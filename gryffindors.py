811365a2044ec169ef75da350ff0ae167d369f34
Arslan Ovezov <arslan.datascience@gmail.com>
2025-05-11 16:23:05 +0500

    ars2

--- gryffindors.py
+++ gryffindors.py
@@ -27,7 +27,7 @@
 
 students = ["Hermione", "Harry", "Ron"]
 
-gryffindors = [{"name": student, "house": "Gryffindor"} for student in students]
+gryffindors = {student: "Gryffindor" for student in students}
 
 print(gryffindors)
 
